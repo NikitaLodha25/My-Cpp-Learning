151392d94956fc0ff1798aa4f64a4ad9c5f1c847
Nikita_Lodha <84386819+NikitaLodha25@users.noreply.github.com>
2023-01-30 13:11:35 +0530

    Update Longest Common Subsequence.cpp

--- DYNAMIC PROGRAMMING/Longest Common Subsequence.cpp
+++ DYNAMIC PROGRAMMING/Longest Common Subsequence.cpp
@@ -5,6 +5,8 @@
 // For example, "ace" is a subsequence of "abcde".
 // A common subsequence of two strings is a subsequence that is common to both strings.
 
+
+// MEMORIZATION :
 class Solution {
 public:
     int longestCommonSubsequence(string &text1, string &text2,int i,int j,int dp[][1001]){
@@ -27,4 +29,25 @@ public:
     }
     
     
+};
+
+
+// DYNAMIC PROGRAMMING :
+class Solution {
+public:
+   
+    int longestCommonSubsequence(string text1, string text2) {
+        int m=text1.size(),n=text2.size();
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+        for(int i=1;i<m+1;i++){
+            for(int j=1;j<n+1;j++){
+               if(text1[i-1]==text2[j-1]){
+                  dp[i][j]= 1+dp[i-1][j-1];
+               }else{
+                   dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+               } 
+            }
+        }
+        return dp[m][n];
+    }
 };
